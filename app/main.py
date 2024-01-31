a28cfeb8d0f66740cbd8c5e80d80547567ee2306
Felipe Ortiz <andresfetiz@gmail.com>
2024-01-31 13:11:25 -0500

    Se agregan cors (#4)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 from app.routers import users,library,security,user_recomendation
 
 
@@ -9,3 +11,11 @@ app.include_router(library.router, prefix="/library", tags=["library"])
 app.include_router(user_recomendation.router, prefix="/recomendation", tags=["recomendation"])
 app.include_router(security.router, prefix="/security", tags=["security"])
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+app.add_middleware(GZipMiddleware)
