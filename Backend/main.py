8de255e5ab070a4dd3572bab24e1224c9b28643c
Felipe Ortiz <andresfetiz@gmail.com>
2024-01-30 22:28:14 -0500

    Delete Backend directory

--- Backend/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from routers import users,library,security,user_recomendation
-
-app = FastAPI()
-
-app.include_router(users.router, prefix="/user", tags=["user"])
-app.include_router(library.router, prefix="/library", tags=["library"])
-app.include_router(user_recomendation.router, prefix="/recomendation", tags=["recomendation"])
-app.include_router(security.router, prefix="/security", tags=["security"])
--- Backend/models/user_recomendation_model.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-from pydantic import BaseModel as PydanticBaseModel, validator
-from pydantic import BaseModel
-
-class UserRecomendation(BaseModel):
-    user_id: int
-    date: datetime 
